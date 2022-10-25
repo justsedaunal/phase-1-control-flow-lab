777eda9569d667c673c5fd6dea66b9a08efdf87e
Seda Ünal <sedaylmz06@outlook.com>
2022-10-25 16:52:23 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num) {
   // Write your code here!
+  if (num <= 400) {
+    return "This one is on me!";
+  } else if (num > 400 && num <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (num > 2500) {
+    return "No can do.";
+  } else if (num > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city == `NYC` ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+// function getFee(isMember) {
+//   return isMember ? "$2.00" : "$10.00";
+// }
+
+function switchOnCharmFromTip(type) {
   // Write your code here!
+  switch (type) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    case "thanks for everything":
+      return "Bye.";
+  }
 }
